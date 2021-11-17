0d9db2454e7a9c89b34bdce8a1bb4ced19ca7960
Radu Dumitrescu <d.raduandrei@gmail.com>
2021-11-17 22:28:02 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,29 @@ import { AppModule } from './app.module';
 import * as helmet from 'helmet';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
-
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+dotenv.config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  
+// if (process.dotenv.APPSTATE == "prod") {
+    
+//   const fs = require('fs');
+//   const keyFile  = fs.readFileSync(__dirname + '/../ssl/mydomain.com.key.pem');
+//   const certFile = fs.readFileSync(__dirname + '/../ssl/mydomain.com.crt.pem');
 
+//   const app = await NestFactory.create(AppModule, {
+//     httpsOptions: {
+//       key: keyFile,
+//       cert: certFile,
+//     }});
+
+// } else {
+    const app = await NestFactory.create(AppModule);
+// }
+
+  
+  
   app.enableCors();
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({
